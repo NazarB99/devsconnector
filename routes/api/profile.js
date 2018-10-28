ce0f01df407d5c0dc11e0d94c7cbf85a03e5da86
NazarB99 <nazar99bayramsahatov@gmail.com>
2018-10-28 19:42:55 +0500

    upd

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.use('/test',(req,res) => res.json({msg:'Profile Works'}));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+router.get('/test',(req,res) => res.json({msg:'Profile Works'}));
+
+router.get('/',passport.authenticate('jwt',{session:false}), (req,res) =>{
+    const errors = {};
+
+    Profile.findOne({user:req.user.id})
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile';
+                return res.status(404).json(errors)
+            }
+            return res.json(profile);
+        })
+
+});
 
 module.exports = router;
