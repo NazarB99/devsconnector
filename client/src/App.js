10d80edd5f5f001536c60d08673bef4879c1dc6c
NazarB99 <nazar99bayramsahatov@gmail.com>
2018-11-09 18:04:29 +0500

    upd

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import Navbar from './components/src/Navbar';
 import Footer from './components/src/Footer';
 import Landing from './components/src/Landing';
+import Login from './components/src/auth/Login';
+import Register from './components/src/auth/Register';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Navbar/>
-          <Landing/>
-          <Footer/>
-      </div>
+      <Router>
+          <div className="App">
+              <Navbar/>
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register}/>
+              <Footer/>
+          </div>
+      </Router>
     );
   }
 }
